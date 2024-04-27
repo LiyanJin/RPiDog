ab8e831ae7846c103b32adc7cedf6fc7463a9988
Hans Jin <hans_jin@126.com>
2024-04-27 22:14:38 +0800

    Create download.sh

---
+++ download.sh
@@ -0,0 +1,3 @@
+
+curl -sSf https://raw.githubusercontent.com/WasmEdge/WasmEdge/master/utils/install.sh | bash -s -- --plugin wasi_nn-ggml
+curl -LO https://github.com/second-state/LlamaEdge/releases/latest/download/llama-chat.wasm
